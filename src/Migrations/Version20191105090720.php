f658735c31251a8c09b18a87e3dbc99d43622eff
alexandrebersihand <a.bersihand@hotmail.fr>
2019-11-05 11:59:11 +0100

    modif CSS, control d'accès

--- src/Migrations/Version20191105090720.php
+++ src/Migrations/Version20191105090720.php
@@ -10,7 +10,7 @@ use Doctrine\Migrations\AbstractMigration;
 /**
  * Auto-generated Migration: Please modify to your needs!
  */
-final class Version20191104153220 extends AbstractMigration
+final class Version20191105090720 extends AbstractMigration
 {
     public function getDescription() : string
     {
@@ -22,7 +22,9 @@ final class Version20191104153220 extends AbstractMigration
         // this up() migration is auto-generated, please modify it to your needs
         $this->abortIf($this->connection->getDatabasePlatform()->getName() !== 'mysql', 'Migration can only be executed safely on \'mysql\'.');
 
-        $this->addSql('CREATE TABLE user (id INT AUTO_INCREMENT NOT NULL, email VARCHAR(180) NOT NULL, roles JSON NOT NULL, password VARCHAR(255) NOT NULL, first_name VARCHAR(50) NOT NULL, last_name VARCHAR(50) NOT NULL, UNIQUE INDEX UNIQ_8D93D649E7927C74 (email), PRIMARY KEY(id)) DEFAULT CHARACTER SET utf8mb4 COLLATE utf8mb4_unicode_ci ENGINE = InnoDB');
+        $this->addSql('ALTER TABLE definition ADD author_id INT NOT NULL');
+        $this->addSql('ALTER TABLE definition ADD CONSTRAINT FK_68302FD8F675F31B FOREIGN KEY (author_id) REFERENCES user (id)');
+        $this->addSql('CREATE INDEX IDX_68302FD8F675F31B ON definition (author_id)');
     }
 
     public function down(Schema $schema) : void
@@ -30,6 +32,8 @@ final class Version20191104153220 extends AbstractMigration
         // this down() migration is auto-generated, please modify it to your needs
         $this->abortIf($this->connection->getDatabasePlatform()->getName() !== 'mysql', 'Migration can only be executed safely on \'mysql\'.');
 
-        $this->addSql('DROP TABLE user');
+        $this->addSql('ALTER TABLE definition DROP FOREIGN KEY FK_68302FD8F675F31B');
+        $this->addSql('DROP INDEX IDX_68302FD8F675F31B ON definition');
+        $this->addSql('ALTER TABLE definition DROP author_id');
     }
 }
