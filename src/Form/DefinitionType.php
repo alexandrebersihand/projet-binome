3fdb3da0d33cfcb6e15afe1ec2141c536d892df5
alexandrebersihand <a.bersihand@hotmail.fr>
2019-11-25 15:48:11 +0100

    modif css, probleme roles/role

--- src/Form/DefinitionType.php
+++ src/Form/DefinitionType.php
@@ -4,6 +4,8 @@ namespace App\Form;
 
 use App\Entity\Definition;
 use Symfony\Component\Form\AbstractType;
+use Symfony\Component\Form\Extension\Core\Type\TextareaType;
+use Symfony\Component\Form\Extension\Core\Type\TextType;
 use Symfony\Component\Form\FormBuilderInterface;
 use Symfony\Component\OptionsResolver\OptionsResolver;
 
@@ -12,15 +14,26 @@ class DefinitionType extends AbstractType
     public function buildForm(FormBuilderInterface $builder, array $options)
     {
         $builder
-            ->add('nom')
-            ->add('language')
-            ->add('content')
-        ;
+            ->add('Nom', TextType::class, array(
+            'label' => $options['Nom'],
+        ))
+            ->add('Language', TextType::class, array(
+                'label' => $options['Language'],
+            ))
+            ->add('Content', TextareaType::class, array(
+                'label' => $options['Content'],
+                'attr' => array(
+                    'placeholder' => 'Inserer votre définition'
+            )));
     }
 
     public function configureOptions(OptionsResolver $resolver)
     {
+
         $resolver->setDefaults([
+            'Nom' => 'Nom de la balise:' ,
+            'Language' => 'Type de langage :',
+            'Content' => 'Définition :',
             'data_class' => Definition::class,
         ]);
     }
